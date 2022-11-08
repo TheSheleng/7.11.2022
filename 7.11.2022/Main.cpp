8fe57eae31dfc938db73426590463b2b0dac5b4f
TheSheleng <sheleng55@gmail.com>
2022-11-09 01:18:49 +0200

    Добавьте файлы проекта.

---
+++ 7.11.2022/Main.cpp
@@ -0,0 +1,76 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+#define DEFAULT_STR_LEN 100
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, (DLGPROC)DlgProc); 
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+
+	case  WM_COMMAND:
+		if (LOWORD(wParam) == IDC_BUTTON)
+		{
+			int result = 0;
+			static const int SCORE_PER_ANSW = 20;
+
+			//проверка первых вопросов
+			static enum
+			{
+				NO_ANSW,
+
+				ANSW_1,
+				ANSW_2,
+				ANSW_3,
+				ANSW_4,
+
+				ANSW_END
+			} RAD_ANSW[(LAST_RADIO - FIRST_RADIO + 1) / (ANSW_END - 1)] = {
+					ANSW_2, ANSW_4, ANSW_2, ANSW_4, ANSW_2 
+				};
+			for (int i = 0; i < ANSW_END; i++)
+			{
+				if (SendDlgItemMessage(
+					hWnd, FIRST_RADIO + i * (ANSW_END - 1) + RAD_ANSW[i] - 1,
+					BM_GETCHECK, NULL, NULL
+				)) result += SCORE_PER_ANSW;
+			}
+
+			//Проверка вторых вопросов
+			static bool CHE_ANSW[LAST_CHECK - FIRST_CHECK + 1] =
+			{
+				false, true, true, false,	 // вопрос 1
+				true, true, false, false,	 // вопрос 2
+				false, true, true, true,	 // вопрос 3
+				true, false, true, false,	 // вопрос 4
+				false, true, false, true,	 // вопрос 5
+			};
+			for (int i = FIRST_CHECK; i <= LAST_CHECK; i++)
+			{
+				if (SendDlgItemMessage(hWnd, i, BM_GETCHECK, NULL, NULL) 
+					== BST_CHECKED == CHE_ANSW[i - FIRST_CHECK]) 
+					result += SCORE_PER_ANSW / (ANSW_END - 1);
+			}
+
+			TCHAR str[DEFAULT_STR_LEN]{};
+			wsprintf(str, TEXT("Результат: %i / %i"), result, 
+				((LAST_RADIO - FIRST_RADIO + 1) + (LAST_CHECK - FIRST_CHECK + 1))
+				/ (ANSW_END - 1) * SCORE_PER_ANSW);
+			MessageBox(hWnd, str, TEXT(""), MB_OK);
+		}
+
+		return TRUE;
+	}
+	return FALSE;
+}
---
+++ 7.11.2022/resource.h
@@ -0,0 +1,66 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется 7.11.2022.rc
+//
+#define IDD_DIALOG                      100
+
+#define IDB_BITMAP1                     102
+#define IDB_BITMAP2                     103
+#define IDC_BUTTON                      104
+
+#define IDC_RADIO1_1                    1001
+#define IDC_RADIO1_2                    1002
+#define IDC_RADIO1_3                    1003
+#define IDC_RADIO1_4                    1004
+#define IDC_RADIO2_1                    1005
+#define IDC_RADIO2_2                    1006
+#define IDC_RADIO2_3                    1007
+#define IDC_RADIO2_4                    1008
+#define IDC_RADIO3_1                    1009
+#define IDC_RADIO3_2                    1010
+#define IDC_RADIO3_3                    1011
+#define IDC_RADIO3_4                    1012
+#define IDC_RADIO4_1                    1013
+#define IDC_RADIO4_2                    1014
+#define IDC_RADIO4_3                    1015
+#define IDC_RADIO4_4                    1016
+#define IDC_RADIO5_1                    1017
+#define IDC_RADIO5_2                    1018
+#define IDC_RADIO5_3                    1019
+#define IDC_RADIO5_4                    1020
+#define FIRST_RADIO                     IDC_RADIO1_1
+#define LAST_RADIO                      IDC_RADIO5_4
+
+#define IDC_CHECK1_1                    2001
+#define IDC_CHECK1_2                    2002
+#define IDC_CHECK1_3                    2003
+#define IDC_CHECK1_4                    2004
+#define IDC_CHECK2_1                    2005
+#define IDC_CHECK2_2                    2006
+#define IDC_CHECK2_3                    2007
+#define IDC_CHECK2_4                    2008
+#define IDC_CHECK3_1                    2009
+#define IDC_CHECK3_2                    2010
+#define IDC_CHECK3_3                    2011
+#define IDC_CHECK3_4                    2012
+#define IDC_CHECK4_1                    2013
+#define IDC_CHECK4_2                    2014
+#define IDC_CHECK4_3                    2015
+#define IDC_CHECK4_4                    2016
+#define IDC_CHECK5_1                    2017
+#define IDC_CHECK5_2                    2018
+#define IDC_CHECK5_3                    2019
+#define IDC_CHECK5_4                    2020
+#define FIRST_CHECK                     IDC_CHECK1_1
+#define LAST_CHECK                      IDC_CHECK5_4
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
