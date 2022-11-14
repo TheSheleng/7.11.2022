6ec1d17e8513fec90e1743c91acea4872c501fb9
TheSheleng <sheleng55@gmail.com>
2022-11-14 11:11:41 +0200

    1

--- 7.11.2022/Main.cpp
+++ 7.11.2022/Main.cpp
@@ -1,10 +1,27 @@
 #include <windows.h>
-#include <tchar.h>
+#include <commctrl.h>
 #include "resource.h"
 
 #define DEFAULT_STR_LEN 100
 
+enum ANSW
+{
+	NO_ANSW,
+
+	ANSW_1,
+	ANSW_2,
+	ANSW_3,
+	ANSW_4,
+
+	ANSW_END
+};
+
+const int cFQues = 5;
+const int cSQues = 5;
+const int cTQues = 2;
+
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+HWND hProg;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {
@@ -14,29 +31,64 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	TCHAR str[DEFAULT_STR_LEN]{};
+
 	switch (message)
 	{
+	case WM_INITDIALOG:
+	{
+		hProg = GetDlgItem(hWnd, IDC_PROGRESS1);
+
+		SendMessage(hProg, PBM_SETRANGE, 0, MAKELPARAM(0, cFQues + cSQues + cTQues));
+		SendMessage(hProg, PBM_SETSTEP, 1, 0);
+		SendMessage(hProg, PBM_SETBARCOLOR, 0, LPARAM(RGB(100, 200, 100)));
+	}
+	return TRUE;
+
 	case  WM_COMMAND:
+	{
+		//Проверка прогреса
+		int progress = 0;
+
+		//Первые вопроссы
+		for (int i = 0; i < cFQues; i++)
+			for (int y = 0; y < ANSW_END; y++)
+				if (SendDlgItemMessage(
+					hWnd, FIRST_RADIO + i * (ANSW_END - 1) + y,
+					BM_GETCHECK, NULL, NULL)) 
+				{ progress += 1; break; }
+
+		//Вторые вопросы
+		for (int i = 0; i < cSQues; i++)
+		{
+			for (int y = 0; y < ANSW_END; y++)
+				if (SendDlgItemMessage(
+					hWnd, FIRST_CHECK + i * (ANSW_END - 1) + y,
+					BM_GETCHECK, NULL, NULL))
+				{
+					progress += 1; break;
+				}
+		}
+
+		//Третие вопросы
+		for (int i = FIRST_EDIT; i <= LAST_EDIT; i++)
+		{
+			GetWindowText(GetDlgItem(hWnd, i), str, DEFAULT_STR_LEN);
+			if (str[NULL] != '\0') progress += 1;
+		}
+
+		SendMessage(hProg, PBM_SETPOS, progress, 0);
+
+		//Проверка теста
 		if (LOWORD(wParam) == IDC_BUTTON)
 		{
 			int result = 0;
 			static const int SCORE_PER_ANSW = 20;
 
 			//проверка первых вопросов
-			static enum
-			{
-				NO_ANSW,
-
-				ANSW_1,
-				ANSW_2,
-				ANSW_3,
-				ANSW_4,
-
-				ANSW_END
-			} RAD_ANSW[(LAST_RADIO - FIRST_RADIO + 1) / (ANSW_END - 1)] = {
-					ANSW_2, ANSW_4, ANSW_2, ANSW_4, ANSW_2 
-				};
-			for (int i = 0; i < ANSW_END; i++)
+			ANSW RAD_ANSW[cFQues] = {
+					ANSW_2, ANSW_4, ANSW_2, ANSW_4, ANSW_2
+			};
+			for (int i = 0; i < cFQues; i++)
 			{
 				if (SendDlgItemMessage(
 					hWnd, FIRST_RADIO + i * (ANSW_END - 1) + RAD_ANSW[i] - 1,
@@ -45,7 +97,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 
 			//Проверка вторых вопросов
-			static const bool CHE_ANSW[LAST_CHECK - FIRST_CHECK + 1] =
+			static const bool CHE_ANSW[cSQues * (ANSW_END - 1)] =
 			{
 				false, true, true, false,	 // вопрос 1
 				true, true, false, false,	 // вопрос 2
@@ -55,15 +107,15 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			};
 			for (int i = FIRST_CHECK; i <= LAST_CHECK; i++)
 			{
-				if (SendDlgItemMessage(hWnd, i, BM_GETCHECK, NULL, NULL) 
-					== BST_CHECKED == CHE_ANSW[i - FIRST_CHECK]) 
+				if (SendDlgItemMessage(hWnd, i, BM_GETCHECK, NULL, NULL)
+					== BST_CHECKED == CHE_ANSW[i - FIRST_CHECK])
 					result += SCORE_PER_ANSW / (ANSW_END - 1);
 			}
 
 			//проверка третих вопросов
-			static const TCHAR EDIT_ANSW[][DEFAULT_STR_LEN] = { 
-				TEXT("120"), 
-				TEXT("156"), 
+			static const TCHAR EDIT_ANSW[cTQues][DEFAULT_STR_LEN] = {
+				TEXT("120"),
+				TEXT("156"),
 			};
 
 			for (int i = FIRST_EDIT; i <= LAST_EDIT; i++)
@@ -74,7 +126,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 
 			//Вывод
-			wsprintf(str, TEXT("Результат: %i / %i"), result, 
+			wsprintf(str, TEXT("Результат: %i / %i"), result,
 				((LAST_RADIO - FIRST_RADIO + 1) +
 				(LAST_CHECK - FIRST_CHECK + 1)) *
 				SCORE_PER_ANSW / (ANSW_END - 1) +
@@ -84,7 +136,8 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 			MessageBox(hWnd, str, TEXT(""), MB_OK);
 		}
-		return TRUE;
+	}
+	return TRUE;
 
 	case WM_CLOSE:
 		EndDialog(hWnd, 0);
--- 7.11.2022/resource.h
+++ 7.11.2022/resource.h
@@ -59,13 +59,15 @@
 #define FIRST_EDIT                      IDC_EDIT1
 #define LAST_EDIT                       IDC_EDIT2
 
+#define IDC_PROGRESS1                   1022
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_CONTROL_VALUE         1023
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
